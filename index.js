ba047c94aba07df42f45307754385db44c7c64b4
Harshxz62 <65876047+Harshxz62@users.noreply.github.com>
2020-06-20 17:05:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+
+for(var i=0;i<7;i++){
+	document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+		var temp = this.innerHTML;
+		makeSound(temp);
+
+		buttonAnimation(temp);
+	})
+}
+
+document.addEventListener("keypress",function(event){
+	makeSound(event.key);
+	buttonAnimation(event.key);
+})
+
+function makeSound(temp){
+	switch(temp){
+		case "w" :
+			var tom1 = new Audio("sounds/tom-1.mp3");
+			tom1.play();
+			break;
+		case "a" :
+			var tom2 = new Audio("sounds/tom-2.mp3");
+			tom2.play();
+			break;
+		case "s" :
+			var tom3 = new Audio("sounds/tom-3.mp3");
+			tom3.play();
+			break;
+		case "d" :
+			var tom4 = new Audio("sounds/tom-4.mp3");
+			tom4.play();
+			break;
+		case "j" :
+			var kickBass = new Audio("sounds/kick-bass.mp3");
+			kickBass.play();
+			break;
+		case "k" :
+			var crash = new Audio("sounds/crash.mp3");
+			crash.play();
+			break;
+		case "l" :
+			var snare = new Audio("sounds/snare.mp3");
+			snare.play();
+			break;
+		default :
+			console.log(temp);
+	}
+}
+
+function buttonAnimation(key){
+	var buttonClick = document.querySelector("."+key);
+	buttonClick.classList.add("pressed");
+
+	setTimeout(function(){
+		buttonClick.classList.remove("pressed");
+	},100);
+}
